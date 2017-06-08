6f284f61213f753a3778c742c40c05640946dbe6
gross-micah <grossmicah@Micahs-MacBook-Pro.local>
2017-06-08 16:44:02 -0700

    outline of connection done

--- opt_enc_d.c
+++ opt_enc_d.c
@@ -27,37 +27,51 @@ int main(int argc, char** argv)
   //define variables
   int charsWritten;
   int charsRead;
-  struct sockaddr_in serverAddress;
+  int listenSocketFD, establishedConnectionFD;
+  struct sockaddr_in serverAddress, clientAddress;
+  socklen_t sizeOfClientInfo;
   memset((char*)&serverAddress, '\0', sizeof(serverAddress));
-  struct hostent* serverHostInfo;
+  //struct hostent* serverHostInfo;
   char buffer[256];
   serverAddress.sin_family = AF_UNIX;
   serverAddress.sin_port = htons(portNumber);
-  serverHostInfo = gethostbyname(localhost);
+  /* serverHostInfo = gethostbyname(localhost);
   if (serverHostInfo == NULL)
   {
     fprintf(stderr, "Error (client): No such host\n");
     exit(0)
   }
-
-  memcpy((char*)&serverAddress.sin_addr.s_addr, (char*)serverHostInfo->h_addr, serverHostInfo->h_length);
+  */
+  //memcpy((char*)&serverAddress.sin_addr.s_addr, (char*)serverHostInfo->h_addr, serverHostInfo->h_length);
 
   //establish socket
-  int socketFD = socket(AF_UNIX, SOCK_STREAM, 0);
-  if (socketFD == -1)
+  listenSocketFD = socket(AF_UNIX, SOCK_STREAM, 0);
+  if (listenSocketFD < 0)
   {
     printf("Error: failed to create socket\n");
     exit(1);
   }
-
-  if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)))
+  //setup listening on socket
+  if (bind(listenSocketFD, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)
   {
-    printf("Error: failed to connect\n");
-    exit(1);
+    error("Error: binding failed\n");
   }
+  listen(listenSocketFD, 5);
 
-  
+  sizeOfClientInfo = sizeof(clientAddress);
+  establishedConnectionFD = accept(listenSocketFD, (struct sockaddr *)&clientAddress, &sizeOfClientInfo);
+  //check if connection accepted
+  if (establishedConnectionFD < 0)
+  {
+    error("Error: failed to acept\n");
+  }
+  //do encryption here
 
+
+  //send it back here
+
+  close(establishedConnectionFD);
+  close(listenSocketFD);
 
   return 0;
 }
