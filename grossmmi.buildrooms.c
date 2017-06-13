dad310d530ad7f307b5a7a305c9571d5afd5ba0a
gross-micah <gross.micah@gmail.com>
2017-06-12 18:28:03 -0700

    Got room connections working

--- grossmmi.buildrooms.c
+++ grossmmi.buildrooms.c
@@ -8,7 +8,7 @@
 enum type {start, mid, end};
 
 struct Room {
-  char roomName[100];
+  char *roomName;
   int connections[7];
   int position;
   int connectionsCount;
@@ -22,16 +22,13 @@ int main()
   //setup room structures
   char *rooms[10] = {"bathroom", "kitchen", "Chipotle", "bedroom", "office",
   "solarium", "garage", "library", "moat", "basement"};
-
   const char *typeString[3] = {"START_ROOM", "MID_ROOM", "END_ROOM"};
 
-  struct Room gameboard[10];
+  struct Room gameboard[7];
   int i, j, k;
-  int staken = 0;
-  int etaken = 0;
   int taken[10];
   //initialize array to ensure no repeated names
-  for (i = 0; i < 7; i++)
+  for (i = 0; i < 10; i++)
   {
     taken[i] = 0;
   }
@@ -45,76 +42,34 @@ int main()
   for (i = 0; i < 7; i++)
   {
     //ensure no garbage in the arrays for values and names
-    memset(gameboard[i].roomName, '\0', 100);
+    //memset(gameboard[i].roomName, '\0', 100);
     memset(gameboard[i].connections, 0, 7);
     int done = 0;
     int temp;
     do {
       temp = rand() % 10;
       //take name from rooms array if available
+
       if (taken[temp] == 0)
       {
-        strcpy(gameboard[i].roomName, rooms[temp]);
+        gameboard[i].roomName = rooms[temp];
         taken[temp] = 1;
         done = 1;
       }
     } while(done != 1);
-    done = 0;
 
-    //set room type
-    do {
-      temp = rand() % 3;
-      if (temp == 0)
-      {
-        if (staken == 0)
-        {
-          gameboard[i].myType = start;
-          staken = 1;
-          done = 1;
-
-        }
-        else if (etaken == 0) // if still missing end, apply here
-        {
-          gameboard[i].myType = end;
-          etaken = 1;
-          done = 1;
-        }
-      }
-      else if (temp == 2)
-      {
-        if (etaken == 0)
-        {
-          gameboard[i].myType = end;
-          etaken = 1;
-          done = 1;
-        }
-        //take another opportunity to try to fill in start and end types
-        else if (staken == 0)
-        {
-          gameboard[i].myType = start;
-          staken = 1;
-          done = 1;
-        }
-      }
-      //prevent fail scenario of missing start and end rooms
-      else if (i == 5 && staken == 0 && etaken == 0)
-      {
-        gameboard[i].myType = start;
-        staken = 1;
-        done = 1;
-      }
-      else if (i == 6 && etaken == 0)
-      {
-        gameboard[i].myType = end;
-        etaken = 1;
-        done = 1;
-      }
-      else
-      {
-        gameboard[i].myType = mid;
-        done = 1;
-      }
-    } while(done != 1);
+    if (i == 0)
+    {
+      gameboard[i].myType = start;
+    }
+    else if (i == 1)
+    {
+      gameboard[i].myType = end;
+    }
+    else
+    {
+      gameboard[i].myType = mid;
+    }
 
     done = 0;
     //create connections between 3 and 6
@@ -186,6 +141,7 @@ int main()
     write = fwrite(nline, sizeof(char), sizeof(nline), fp);
 
     //loop through array of connections. AND statement sets it to ignore itself in connections
+    //note: this is nested. The room we care about is of value i. the value j is for its connection.
     int count = 1;
     for (j = 0; j < 7; j++)
     {
@@ -195,7 +151,7 @@ int main()
         char num[2];
         for (j = 0; j < 2; j++)
         {
-          num[i] = '\0';
+          num[j] = '\0';
         }
         k = sprintf(num, "%d", i);
         count++;
